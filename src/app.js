87827210abc3c9b2cbea3d54d53b8d20dcd8f335
Scott Chapman <scottchapman@us.ibm.com>
2017-03-25 17:01:17 -0400

    Fixed bug

--- src/app.js
+++ src/app.js
@@ -34,8 +34,8 @@ export const echo = (appId, token) => (req, res) => {
     // return;
 
 	io.sockets.emit('webhook-event', {eventTime: new Date(), body: req.body});
+};
 
-  })
   // log('SpaceID: %s', req.body.spaceId);
   // log('SpaceName: %s', req.body.spaceName);
   // log('userName: %s', req.body.userName);
