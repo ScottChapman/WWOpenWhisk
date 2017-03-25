b7dc812f570c8deacbed95280dca0764b8eff4d7
Scott Chapman <scottchapman@us.ibm.com>
2017-03-25 16:57:48 -0400

    Removed old code

--- src/app.js
+++ src/app.js
@@ -17,50 +17,6 @@ import * as fs from 'fs';
 import * as _ from 'lodash';
 var io;
 
-function genActionClick(text,title,buttons) {
-// Generate template annotation
-  const annotations = util.format(`
-    {
-      genericAnnotation: {
-        title: "%s"
-        text: "%s"
-        color: "#6CB7FB"
-          actor: {
-            name: "HealthCare Bot V1.0"
-            avatar: "https://scwatsonwork-echo.mybluemix.net/bot.png"
-            url: "https://github.com/watsonwork/watsonwork-echo"
-          }
-        buttons: [ %s ]
-      }
-    }`, title, text,
-      // Generate buttons
-      (buttons || []).map((b) => util.format(`{
-        postbackButton: {
-          title: "%s",
-          id: "%s",
-          style: %s
-        }
-      }`, b[0], b[1], b[2] || 'PRIMARY')).join(','));
-    return annotations;
-}
-
-function genMutation(annotations,actionAnnotation) {
-  var payload = actionAnnotation.annotationPayload;
-  var str = util.format(`
-      mutation {
-        createTargetedMessage(input: {
-          conversationId: "%s"
-          targetUserId: "%s"
-          targetDialogId: "%s"
-          annotations: [ %s ]
-        }) {
-          successful
-        }
-      }`, actionAnnotation.spaceId, actionAnnotation.userId, payload.actionId, annotations);
-  console.log("Mutation: " + str);
-  return str;
-}
-
 // Debug log
 const log = debug('watsonwork-echo-app');
 
@@ -74,47 +30,11 @@ export const echo = (appId, token) => (req, res) => {
   // Only handle message-created Webhook events, and ignore the app's
   // own messages
   // if(req.body.type !== 'message-annotation-added')
-  if(req.body.type !== 'message-annotation-added')
-    return;
+  // if(req.body.type !== 'message-annotation-added')
+    // return;
 
 	io.sockets.emit('webhook-event', {eventTime: new Date(), body: req.body});
 
-  req.body.annotationPayload = JSON.parse(req.body.annotationPayload);
-
-  console.log("AnnotationType: %s", req.body.annotationType);
-
-  if(req.body.annotationPayload.applicationId !== appId)
-    return;
-
-  if (req.body.annotationType === "actionSelected") {
-    log("Got an action selected event!");
-    var annotations = genActionClick("MyText","MyTitle",[["first","1","PRIMARY"],["second","2","SECONDARY"]]);
-    log("Annotation: %s", annotations);
-    var mutation = genMutation(annotations,req.body);
-    log("Mutation: %s", mutation);
-  	io.sockets.emit('debug-event', mutation);
-    graphQL(token(), mutation, (err,res) => {
-      if(!err) {
-        log('Got graphQL Response back! %o', res.body);
-      }
-      else {
-        log("Error with graphQL request... %o", err)
-      }
-    });
-  }
-
-  var command = getHCCommand(req.body);
-  log('Got a Lens %o', command);
-
-  // Send the echo message
-  generateResponse(command, resp => {
-    send(req.body.spaceId,
-        resp,
-      token(),
-      (err, res) => {
-        if(!err)
-          log('Sent message to space %s', req.body.spaceId);
-      });
   })
   // log('SpaceID: %s', req.body.spaceId);
   // log('SpaceName: %s', req.body.spaceName);
@@ -153,129 +73,6 @@ export const echo = (appId, token) => (req, res) => {
       // });
 };
 
-function getLabResult(command, callback) {
-  if (!command.hasOwnProperty('entities')) return null;
-  console.log("getLabResults!");
-  console.dir(command.entities);
-  command.entities.forEach(function (entity) {
-    console.dir(entity);
-    if (entity.source === "Conversation" &&
-      ((entity.type === "vitals") || (entity.type === 'labtest') || (entity.type === 'bloodtest') || (entity.type === 'condition'))) {
-      console.log("Got an entity that is vitals: ", entity.text);
-      switch(entity.text) {
-          case 'systolic':
-          case 'Hypertension':
-          case 'diastolic':
-          case 'blood pressure':
-              console.log("Blood pressure!");
-              callback(
-                {
-                  command: "Lab Result",
-                  message: "Blood pressure was 140/85 when last checked on Dec. 12, 2014"
-                }
-              );
-              break;
-          case 'blood sugar':
-          case 'Diabetes':
-          case 'Glucose':
-              console.log("Blood sugar!");
-              callback(
-                {
-                  command: "Lab Result",
-                  message: "A1C was 6.8% and blood sugar was 126 mg/dL when last checked on Dec. 12, 2014"
-                }
-              );
-              break;
-          default:
-              console.log("unknown!");
-              callback(
-                {
-                  command: "Lab Result",
-                  message: util.format("We don't have any lab results for %s.", entity.text)
-                }
-              );
-      }
-    }
-  })
-}
-
-function getLabRequest(command, callback) {
-  if (!command.hasOwnProperty('entities')) return null;
-  command.entities.forEach(function (entity) {
-    if (entity.source === "Conversation" &&
-      ((entity.type === "vitals") || (entity.type === 'labtest') || (entity.type === 'bloodtest') || (entity.type === 'condition'))) {
-      callback(
-        {
-          command: "Lab Request",
-          message: util.format("I've added a %s test for the next appointment.", entity.text)
-        }
-      );
-    }
-    else {
-      callback(
-        {
-          command: "Lab Request",
-          message: util.format("Sorry, not clear about what test to order.", entity.text)
-        }
-      );
-      }
-  })
-}
-
-function getFollowup(command, callback) {
-  var caregiver = null;
-  var from = null
-  var to = null;
-  command.entities.forEach(function (entity) {
-    switch (entity.type) {
-      case 'caregiver':
-        caregiver = entity.text;
-        break;
-      case 'sys-date':
-        if (from !== null)
-          to = entity.text;
-        else {
-          from = entity.text;
-        }
-    }
-  })
-
-  if (caregiver === null) caregiver = "primary care";
-  if (to !== null)
-      callback(
-        {
-          command: "Followup Appointment",
-          message: util.format("We'll schedule a follow up with %s between %s and %s.", caregiver,from,to)
-        }
-      );
-  else if (from !== null)
-      callback(
-        {
-          command: "Followup Appointment",
-          message: util.format("We'll schedule a follow up with %s on %s.", caregiver,from)
-        }
-      );
-  else
-      callback(
-        {
-          command: "Followup Appointment",
-          message: util.format("We'll schedule a follow up appointment with %s.", caregiver)
-        }
-      );
-}
-
-function generateResponse(command, callback) {
-  if (command.lens === 'Request' && command.hasOwnProperty('category') && command.category === "LabResults") {
-    getLabResult(command,callback);
-  }
-  else if (command.lens === 'Request' && command.hasOwnProperty('category') && command.category === "Labs") {
-    getLabRequest(command,callback);
-  }
-  else if (command.lens === 'Followup') {
-    getFollowup(command,callback);
-  }
-}
-
 const graphQL = (token, body, callback) => {
   request.post(
     'https://watsonwork.ibm.com/graphql', {
@@ -295,38 +92,6 @@ const graphQL = (token, body, callback) => {
       }
       callback(err,response);
     } );
-}
-
-function getHCCommand(cmd) {
-  var object = {};
-
-  if (cmd.hasOwnProperty("annotationPayload")) {
-    var payload = cmd.annotationPayload;
-    // console.log("Lens: " + payload.lens);
-    object.lens = payload.lens;
-    if (payload.hasOwnProperty("category")) {
-      // console.log("Category: " + payload.category);
-      object.category = payload.category;
-    }
-
-    if (payload.hasOwnProperty("extractedInfo")) {
-      var info = payload.extractedInfo;
-      if (info.hasOwnProperty("entities")) {
-        var entities = info.entities;
-        var small = _.map(_.filter(entities,{"source": "Conversation"}),(entity) => {
-          return _.pick(entity,["source","text","type"]);
-        });
-        object.entities = small;
-        // console.dir(small);
-        /*
-        _.filter(entities,{"source": "Conversation"}).forEach((entity) => {
-          console.dir(_.pick(entity,["source","text","type"]))
-        })
-        */
-      }
-    }
-  }
-  return object;
 }
 
 // Send an app message to the conversation in a space
